6cbb3fe5a7fbf48b7eada3c3808a85980bd2f534
DivyaMeghanaMamidipalli <165604629+DivyaMeghanaMamidipalli@users.noreply.github.com>
2024-05-20 12:14:55 +0530

    Add files via upload

---
+++ client/src/utils/constants.js
@@ -0,0 +1,9 @@
+export const CDN_URL=
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
+
+export const LOGO_URL =
+    "https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.logodesign.net%2Flogo%2Fsmoking-burger-with-lettuce-3624ld.png&tbnid=hXdeg1sHFMWVNM&vet=12ahUKEwi9_dKRkbODAxWFk2MGHeFmApgQMygKegQIARBg..i&imgrefurl=https%3A%2F%2Fwww.logodesign.net%2Flogos%2Ffood&docid=DXiwq17-71YNeM&w=405&h=309&q=food%20app%20logo%20ideas&ved=2ahUKEwi9_dKRkbODAxWFk2MGHeFmApgQMygKegQIARBg";
+
+export  const DATA_URL =
+    "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+ 
